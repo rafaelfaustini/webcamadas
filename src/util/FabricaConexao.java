db07917986b2cce70f412c4792221d894f80ab9e
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2019-08-21 19:48:52 -0300

    Aula 04

---
+++ src/util/FabricaConexao.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class FabricaConexao {
+	private Connection conexao;
+	
+	public Connection fazerConexao() {
+		
+		try {
+			Class.forName("org.mysql.Driver");
+			
+			this.conexao = DriverManager.getConnection("jdbc://mysql://localhost:3306/webcamadas",
+					"root",
+					"");
+			
+					
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return this.conexao;
+	}
+	
+	public void fecharConexao() {
+		try {
+			this.conexao.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
