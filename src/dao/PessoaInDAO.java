03e51cf22db73d5a6fdf8addaa3ca80f7546a6fb
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2019-08-21 19:55:28 -0300

    DAO

---
+++ src/dao/PessoaInDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Pessoa;
+
+public interface PessoaInDAO {
+	void Inserir(Pessoa objeto) throws SQLException;
+	
+	List<Pessoa> listarTodos() throws SQLException;
+	
+	Boolean Excluir(int _id) throws SQLException;
+	
+	Boolean Atualizar(Pessoa _objeto) throws SQLException;
+	
+	Pessoa buscarPorId(int _id) throws SQLException;
+}
