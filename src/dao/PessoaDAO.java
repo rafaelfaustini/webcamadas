4b4a8236bdeed816a5f57f3e85f1524aa59e7702
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2019-08-27 19:15:26 -0300

    Inserir Pessoa DAO

--- src/dao/PessoaDAO.java
+++ src/dao/PessoaDAO.java
@@ -1,15 +1,31 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 import model.Pessoa;
 
 public class PessoaDAO implements PessoaInDAO {
-
+	
+	private Connection conexao;
+	
+	public PessoaDAO(Connection _conn) {
+		this.conexao = _conn;
+	}
+	
 	@Override
-	public void Inserir(Pessoa objeto) throws SQLException {
-		// TODO Auto-generated method stub
+	public void Inserir(Pessoa _objeto) throws SQLException {
+		String SQL = "insert into pessoa (id, nome, email) values (?, ?, ?)";
+		
+		PreparedStatement ps = this.conexao.prepareStatement(SQL);
+		
+		ps.setInt(1, _objeto.getId());
+		ps.setString(2, _objeto.getNome());
+		ps.setString(3, _objeto.getEmail());
+		
+		ps.execute();
 
 	}
 
