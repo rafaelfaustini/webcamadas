2c6ed0644095324d6c293afab3dd12cc9f7acaf5
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2019-09-03 19:47:05 -0300

    Servlet de Listar pessoas

---
+++ src/control/ListarPessoasServlet.java
@@ -0,0 +1,63 @@
+package control;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import dao.PessoaDAO;
+import model.Pessoa;
+import util.FabricaConexao;
+
+/**
+ * Servlet implementation class ListarPessoasServlet
+ */
+@WebServlet("/ListarPessoasServlet")
+public class ListarPessoasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListarPessoasServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try{
+			FabricaConexao fabrica = new FabricaConexao();
+			Connection conexao = fabrica.fazerConexao();
+			
+			PessoaDAO dao = new PessoaDAO(conexao);
+			
+			List<Pessoa> listaPessoas = dao.listarTodos();
+			request.setAttribute("ListarPessoas", listaPessoas);
+			
+			RequestDispatcher despachante = request.getRequestDispatcher("/listarpessoas.jsp");
+			despachante.forward(request, response);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
